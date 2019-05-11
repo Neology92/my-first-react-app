2fca80d25d48de3b0d64869ab5a717719954bdea
Neology92 <oskarlegner@gmail.com>
2019-05-11 14:50:43 +0200

    fixed spelling

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,8 +1,8 @@
 import React, {Fragment} from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../logo.png';
-import Modal from '../../Utiles/Modal';
-import Toggle from '../../Utiles/Toggle';
+import Modal from '../../Utils/Modal';
+import Toggle from '../../Utils/Toggle';
 import { HeaderWrapper, Logo } from './style';
 
 
