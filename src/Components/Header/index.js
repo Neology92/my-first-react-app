85d7c5328d57d4fdeb6b9505b40805b75734ff3a
Neology92 <oskarlegner@gmail.com>
2019-05-12 18:53:15 +0200

    Create animated text
    Created animating text to learn basic react-spring's Transition

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,5 +1,6 @@
 import React, {Fragment} from 'react';
 import { Link } from 'react-router-dom';
+import { Transition } from 'react-spring/renderprops';
 import { Toggle } from 'Utils';
 import { Modal } from 'Elements';
 import { HeaderWrapper, Logo } from './style';
@@ -12,6 +13,25 @@ const Header = () => {
       <Link to="/">
         <Logo src={logo} className="App-logo" alt="logo" />  
       </Link>
+
+
+        <Toggle>
+          {({ on, toggle }) => (
+            <Fragment>
+              <Transition
+                items={on}
+                keys={(item => item.key)}
+                from={{opacity: 0}}
+                enter={{opacity: 1}}
+                leave={{opacity: 0}}
+                >
+                {on => on && Showme}
+              </Transition>
+              <button onClick={toggle}>Show/Hide</button>
+            </Fragment>
+          )}
+        </Toggle>
+
 
       <Toggle>
           {({ on, toggle }) => (
@@ -29,5 +49,13 @@ const Header = () => {
     </HeaderWrapper>
   )
 }
+
+const Showme = props => 
+  <p style={{
+    opacity: props.opacity,
+    'font-size': '1.4rem'
+  }}>
+    Show me!
+  </p>;
 
 export default Header;
