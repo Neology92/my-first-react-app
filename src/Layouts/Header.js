bb43755685ad237e91caeda8079cba440f6bca90
Neology92 <oskarlegner@gmail.com>
2019-05-16 21:57:06 +0200

    Simplify and organize code

--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -7,44 +7,42 @@ import { Toggle, darkBackground, borderColor } from 'Utils';
 import { MenuButton } from 'Elements';
 import logo from 'logo.png';
 
-const Header = () => {
-	return (
-		<HeaderWrapper>
-			<Link to='/'>
-				<Logo src={logo} className='App-logo' alt='logo' />
-			</Link>
-			<Toggle>
-				{({ on, toggle }) => (
-					<Fragment>
-						<MenuButton
-							color='white'
-							onClick={toggle}
-							open={on}
-						/>
-						<Transition
-							unique
-							items={on}
-							from={{
-								transform: 'translateX(100%)',
-							}}
-							enter={{
-								transform: 'translateX(0%)',
-							}}
-							leave={{
-								transform: 'translateX(100%)',
-							}}
-						>
-							{on => on && HamburgerMenuList}
-						</Transition>
-					</Fragment>
-				)}
-			</Toggle>
-			<MenuList />
-		</HeaderWrapper>
-	);
-};
+const Header = ({ className }) => (
+	<header className={className}>
+		<Link to='/'>
+			<img src={logo} alt='logo' />
+		</Link>
+		<Toggle>
+			{({ on, toggle }) => (
+				<Fragment>
+					<MenuButton
+						color='white'
+						onClick={toggle}
+						open={on}
+					/>
+					<Transition
+						unique
+						items={on}
+						from={{
+							transform: 'translateX(100%)',
+						}}
+						enter={{
+							transform: 'translateX(0%)',
+						}}
+						leave={{
+							transform: 'translateX(100%)',
+						}}
+					>
+						{on => on && HamburgerMenuList}
+					</Transition>
+				</Fragment>
+			)}
+		</Toggle>
+		<MenuList />
+	</header>
+);
 
-export const HeaderWrapper = styled.div`
+export default styled(Header)`
 	padding: 0 5vw;
 	height: 10vh;
 	display: flex;
@@ -55,12 +53,10 @@ export const HeaderWrapper = styled.div`
 	color: white;
 	background-color: ${darkBackground};
 	border: solid 1px ${borderColor};
-`;
 
-export const Logo = styled.img`
-	height: 4rem;
-	color: white;
-	pointer-events: none;
+	& img {
+		height: 4rem;
+		color: white;
+		pointer-events: none;
+	}
 `;
-
-export default Header;
