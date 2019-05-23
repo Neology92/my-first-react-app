2ee1fb99da8eca08ca1a62a0915c2ec40606d3be
Neology92 <oskarlegner@gmail.com>
2019-05-23 23:07:51 +0200

    Change Buttons files structure

--- src/Elements/Buttons.js
+++ src/Elements/Buttons.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
+import { applyStyleModifiers } from 'styled-components-modifiers';
 import { above } from 'Utils';
 
-export default class HamburgerButton extends React.Component {
+const BUTTON_MODIFIERS = {
+	submit: () => `
+        background-color: darkgreen;
+        width: 422px;
+        border: 1px solid white;
+    `,
+
+	counter: () => `
+        background-color: #500158;
+        min-width: 70px;
+		padding: 10px;
+		margin: 10px;
+    `,
+};
+
+export const Button = styled.button`
+	padding: 5px;
+	font-weight: 600;
+	text-align: center;
+	line-height: 50px;
+	color: rgb(255, 255, 255, 0.5);
+	border-radius: 5px;
+	background: rgb(97, 22, 22);
+	font-family: 'Josefin Slab', serif;
+	font-size: 20px;
+	line-height: 24px;
+	cursor: pointer;
+	border: none;
+
+	&:disabled:hover {
+		color: rgb(255, 255, 255, 0.5);
+		cursor: default;
+	}
+	&:hover {
+		color: #fff;
+	}
+
+	${applyStyleModifiers(BUTTON_MODIFIERS)}
+`;
+
+/* ================================================= */
+
+export class HamburgerButton extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
--- src/Elements/index.js
+++ src/Elements/index.js
@@ -1,17 +1,9 @@
 import Modal from './Modal';
 import Icon from './Icon';
-import HamburgerButton from './HamburgerButton';
 import MenuOptions from './MenuOptions';
 import MoviePoster from './MoviePoster';
-import Button from './Button';
 
 export * from './MenuList';
 export * from './Card';
-export {
-	Modal,
-	Icon,
-	HamburgerButton,
-	MenuOptions,
-	MoviePoster,
-	Button,
-};
+export * from './Buttons';
+export { Modal, Icon, MenuOptions, MoviePoster };
